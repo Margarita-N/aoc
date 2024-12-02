7b16d800f0c3aa41c7ea72a400fcaa5d05ab5eed
Margarita Nasufi <margarita.nasufi@fresha.com>
2024-12-02 22:48:57 +0100

    Logic for part 2 and refactor to reuse same logic for part 1 and 2

--- lib/days/day_2.ex
+++ lib/days/day_2.ex
@@ -1,11 +1,19 @@
 defmodule Aoc.Days.Day2 do
   def solution_1(input) do
-    Enum.reduce(input, 0, fn report, acc -> acc + validate_report_progress(report) end)
+    Enum.reduce(input, 0, fn report, acc ->
+      case validate_report_progress(report) do
+        {_, 1} -> acc + 1
+        {_, 0} -> acc
+      end
+    end)
   end
 
   defp validate_report_progress(report = [head | [compare | _tail]]) do
-    find_trend(head - compare)
-    |> validate_report_progress(report)
+    is_valid =
+      find_trend(head - compare)
+      |> validate_report_progress(report)
+
+    {report, is_valid}
   end
 
   defp validate_report_progress(trend, report = [head | [compare | tail]]) when length(report) >= 2 do
@@ -22,6 +30,44 @@ defmodule Aoc.Days.Day2 do
 
   defp find_trend(trend) when trend > 0, do: -1 # decreasing
   defp find_trend(_trend), do: 1 # increasing
+
+  def solution_2(input) do
+    result = Enum.map(input, &validate_report_progress/1)
+
+    safe_unsafe =
+      result
+      |> Enum.filter(fn {_, is_valid} -> is_valid == 0 end)
+      |> Enum.reduce(0, fn {report, _}, acc ->
+        case check_error_levels(report, 0) do
+          :safe -> acc + 1
+          :unsafe -> acc
+        end
+      end)
+
+    IO.inspect(safe_unsafe)
+
+    safe =
+      result
+      |> Enum.filter(fn {_, is_valid} -> is_valid == 1 end)
+      |> Enum.count()
+
+    IO.inspect(safe)
+
+    safe + safe_unsafe
+  end
+
+  def check_error_levels(report, index) when index == length(report), do: :unsafe
+  def check_error_levels(report, index) do
+    report
+    |> List.delete_at(index)
+    |> validate_report_progress()
+    |> case do
+      {_, 1} ->
+        :safe # at least once is safe
+      {_, 0} ->
+        check_error_levels(report, index + 1)
+    end
+  end
 
   def parse_input do
     {:ok, contents} = File.read("lib/inputs/day_2.txt")

be4e9dac7cd4af928b27a72263929582c98dc781
Margarita Nasufi <margarita.nasufi@fresha.com>
2024-12-08 21:29:16 +0100

    A very lazy part 2 for day 4

--- lib/days/day_4.ex
+++ lib/days/day_4.ex
@@ -5,11 +5,43 @@ defmodule Aoc.Days.Day4 do
 
   def solution_1(input_matrix) do
     input_matrix
-    |> get_coordinates_for_X()
+    |> get_coordinates_for_center_letter("X")
     |> Enum.flat_map(fn x -> x end)
     |> Enum.reduce(0, fn {i,j},acc ->
         acc + check_square(i, j, input_matrix)
     end)
+  end
+
+  def solution_2(input_matrix) do
+    input_matrix
+    |> get_coordinates_for_center_letter("A")
+    |> Enum.flat_map(fn x -> x end)
+    |> Enum.reduce(0, fn {i,j}, acc ->
+      acc + check_xmas(i,j, input_matrix)
+    end)
+  end
+
+  defp check_xmas(i,j,input_matrix) do
+    elem_1 = get_at(input_matrix,i+1,j+1)
+    elem_2 = get_at(input_matrix,i-1,j-1)
+
+    elem_3 = get_at(input_matrix,i+1,j-1)
+    elem_4 = get_at(input_matrix,i-1,j+1)
+
+    if Enum.all?([elem_1,elem_2,elem_3,elem_4], fn x -> x != nil end) and
+    ([elem_1, elem_2] |> Enum.sort() == ["M", "S"] and [elem_3, elem_4] |> Enum.sort() == ["M", "S"]) do
+      1
+    else
+      0
+    end
+  end
+
+  defp get_at(input_matrix,i,j) do
+    if i >= 0 && i < @grid_size && j >= 0 && j < @grid_size do
+      Enum.at(input_matrix, i) |> Enum.at(j)
+    else
+      nil
+    end
   end
 
   def check_square(i, j, input_matrix) do
@@ -51,10 +83,10 @@ defmodule Aoc.Days.Day4 do
     ]
   end
 
-  def get_coordinates_for_X(input_matrix) do
+  def get_coordinates_for_center_letter(input_matrix, letter) do
     for {row, i} <- Enum.with_index(input_matrix) do
       for {elem, j} <- Enum.with_index(row),
-      elem == "X", do: {i,j}
+      elem == letter, do: {i,j}
     end
   end
 

088f9fde1259bd95842189cdd8f3cca90bf49abd
Margarita Nasufi <margarita.nasufi@fresha.com>
2024-12-09 23:52:16 +0100

    Fixed throwing error for part 1

--- lib/days/day_9.ex
+++ lib/days/day_9.ex
@@ -8,18 +8,18 @@ defmodule Aoc.Days.Day9 do
     |> calculate_sum(0, -1 , 0, count)
   end
 
+  defp calculate_sum(list, _, end_index, acc, _) when length(list) <= abs(end_index), do: acc
   defp calculate_sum(_, start_index, end_index, acc, count) when start_index > (count + end_index), do: acc
 
   defp calculate_sum(remaining_list = [head | tail], start_index, end_index, acc, count) when start_index <= (count + end_index) do
-    IO.inspect({start_index, end_index, acc})
     if head == "." do
       if Enum.at(tail, end_index) != "." do
-        calculate_sum(tail, start_index + 1, end_index - 1, acc + (start_index * Enum.at(tail, end_index)))
+        calculate_sum(tail, start_index + 1, end_index - 1, acc + (start_index * Enum.at(tail, end_index)), count)
       else
-        calculate_sum(remaining_list, start_index, end_index - 1, acc)
+        calculate_sum(remaining_list, start_index, end_index - 1, acc, count)
       end
     else
-      calculate_sum(tail, start_index + 1, end_index, acc + (start_index * head))
+      calculate_sum(tail, start_index + 1, end_index, acc + (start_index * head), count)
     end
   end
 
